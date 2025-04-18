e1ab3f50a666dc6b4704466a14636e45b3df25ad
antoniommc <a103800@alunos.uminho.pt>
2025-04-19 00:50:51 +0100

    1

--- ProjetoPW/BackOffice/HistoricoAuditorias/app.js
+++ ProjetoPW/BackOffice/HistoricoAuditorias/app.js
@@ -97,7 +97,7 @@ new Vue({ // Cria uma nova instância do Vue
       localStorage.setItem('idAuditoria', id)
   
       // Redireciona para a página de Local Auditado
-      window.location.href = "LocalAuditado(HTMLeCSS).html";
+      window.location.href = "LocalAuditado/LocalAuditado(HTMLeCSS).html";
     }
   },
   
