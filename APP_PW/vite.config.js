326a3e96cf9ba508c9e9fa9c962d0132b356a33b
Bacardi101 <a104241@uminho.pt>
2025-05-09 09:26:37 +0100

    .

--- APP_PW/vite.config.js
+++ APP_PW/vite.config.js
@@ -2,11 +2,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    tailwindcss(),
+    tailwindcss()
   ],
-  
+  base: '/app/', // 👈 ESSENCIAL
+  build: {
+    outDir: 'dist',        // default
+    rollupOptions: {
+      input: 'index.html'  // 👈 certifica-te que está aqui
+    }
+  }
 })
