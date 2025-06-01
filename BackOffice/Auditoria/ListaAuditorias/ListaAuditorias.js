5e35ea5d8524caba4a69a4a58b474a661644d599
antoniommc <a103800@alunos.uminho.pt>
2025-06-01 23:15:06 +0100

    correcoes

--- BackOffice/Auditoria/ListaAuditorias/ListaAuditorias.js
+++ BackOffice/Auditoria/ListaAuditorias/ListaAuditorias.js
@@ -325,7 +325,7 @@ function editarAuditoria(id) {
                         ${[
                             "Lanterna", "Câmera fotográfica", "Fita sinalizadora e cones",
                             "Fita métrica", "Luvas de proteção", "Capacete de segurança",
-                            "Caneleiras", "Bloco de notas", "Colete refletor"
+                            "Caneleiras", "Bloco de notas", "Colete reflexor","Tablet"
                         ].map(mat => `
                             <label>
                                 <span>${mat}</span>
